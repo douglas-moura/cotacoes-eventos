da06a7b2abffa1c1f8ee45614f4403db6d51d55a
Douglas Moura <douglas12345.dsm@gmail.com>
2026-03-02 17:34:07 -0300

    feat: implementando navegação entre páginas com React Router e estilos com css com Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-interface MyButtonProps {
-    /** O texto à ser exibido dentro do botão */
-    title: string;
-    /** Se poderá haver interação com o botão */
-    disabled: boolean;
-}
+import Router from "./router"
 
-function MyButton({ title, disabled }: MyButtonProps) {
-    return (
-        <button disabled={disabled}>{title}</button>
-    );
-}
-
-export default function MyApp() {
-    return (
-        <div>
-            <h1>Bem-vindo ao my app</h1>
-            <MyButton title="Eu sou um botão desabilitado" disabled={true} />
-        </div>
-    );
+export default function App() {
+    return <Router />
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="">
+            <h1 className="">
+                Home
+            </h1>
+        </div>
+    )
+}
