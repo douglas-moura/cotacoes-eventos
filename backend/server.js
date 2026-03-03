68bbcc459c5e21592e6e3e6dff39c86eef9c0877
Douglas Moura <douglas12345.dsm@gmail.com>
2026-03-03 11:00:16 -0300

    feat: Backend implementado
    - Foi preciso utilizar o Prisma 6 para conseguir utilizar o Prisma Client, já que a versão 7 ainda não tem suporte para isso.
    - O Prisma Client é uma biblioteca que permite acessar o banco de dados de forma mais fácil
    
    .env → conexão
    schema.prisma → modelo
    migrate → cria tabela
    generate → cria client
    server.js → usa o client

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,24 @@
-// importar a biblioteca Express para criar o servidor
+require('dotenv').config()
+
 const express = require('express')
-// importar a biblioteca CORS para permitir requisições de outros domínios/portas
 const cors = require('cors')
-
 const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
 
-// criar uma instância Servidor do Express
+const prisma = new PrismaClient()
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
-
-// use registra um middleware
-// middleware = função que roda antes da rota
-// é um middleware que permite o servidor entender JSON
+app.use(cors())
 app.use(express.json())
 
-// cria uma rota GET para o caminho '/'
 app.get('/', (req, res) => {
-    res.send('Backend funcionando');
+    res.send('Backend funcionando')
 })
 
-// cria uma rota GET para o caminho '/users'
-app.get('/users', (req, res) => {
-    users = prisma.user.findMany()
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
     res.json(users)
 })
 
-// inicia o servidor na porta 3000
 app.listen(3000, () => {
-    console.log('Backend rodando em http://localhost:3000')
+    console.log('Servidor rodando em http://localhost:3000')
 })
