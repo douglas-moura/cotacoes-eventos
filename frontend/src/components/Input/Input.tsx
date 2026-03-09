69c2e4fbd6a2c5ee9e91e82921d564ed28e298ed
Douglas Moura <douglas12345.dsm@gmail.com>
2026-03-09 15:22:16 -0300

    feat: Implementando funcionalidade ver senha no input de senhas

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Icon } from '@iconify/react'
 import { InputProps } from '../../types/interface'
 import './Input.css'
 
 export default function Input({InputType, inputLabel, placeholder, opcoes, value, status, onChange}: InputProps): React.JSX.Element {
+    const [verSenha, setVerSenha] = useState<boolean>(false)
+
     switch (InputType) {
         case 'text':
             return (
@@ -25,8 +27,8 @@ export default function Input({InputType, inputLabel, placeholder, opcoes, value
             return (
                 <span className='input-container'>
                     <label htmlFor="password">{inputLabel}</label>
-                    <Icon icon="mdi:eye" width="20" className='icone-aux' />
-                    <input type="password" placeholder={placeholder} className={`input-default ${status === false ? 'input-alert' : ''}`} value={value} onChange={(e) => onChange && onChange(e.target.value)} />
+                    <Icon icon="mdi:eye" width="20" className='icone-aux' onClick={() => setVerSenha(!verSenha)} />
+                    <input type={!verSenha ? 'password' : 'text'} placeholder={placeholder} className={`input-default ${status === false ? 'input-alert' : ''}`} value={value} onChange={(e) => onChange && onChange(e.target.value)} />
                     <div id='password-assets' className='text-xs my-2'>
                         <div className="text-gray-500 items-center flex">
                             <input type="checkbox" id="lembrar-password" className="mr-2" />
@@ -42,8 +44,8 @@ export default function Input({InputType, inputLabel, placeholder, opcoes, value
             return (
                 <span className='input-container'>
                     <label htmlFor="password">{inputLabel}</label>
-                    <Icon icon="mdi:eye" width="20" className='icone-aux' />
-                    <input type="password" placeholder={placeholder} className={`input-default ${status === false ? 'input-alert' : ''}`} value={value} onChange={(e) => onChange && onChange(e.target.value)} />
+                    <Icon icon="mdi:eye" width="20" className='icone-aux' onClick={() => setVerSenha(!verSenha)} />
+                    <input type={!verSenha ? 'password' : 'text'} placeholder={placeholder} className={`input-default ${status === false ? 'input-alert' : ''}`} value={value} onChange={(e) => onChange && onChange(e.target.value)} />
                     {status == false && <p className='text-[.5rem] mt-1 text-red-500'>{inputLabel == 'Senha' ? 'A senha deve conter pelo menos 8 caracteres, incluindo uma letra e um número' : 'As senhas precisam ser iguais'}</p>}
                 </span>
             )
